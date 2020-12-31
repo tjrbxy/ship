723ce1e1bdc4503443d1ab7b25985c48aaa17fcc
abel.tang <972980756@qq.com>
2020-12-31 17:18:50 +0800

    1.加入 lombok 2.关闭文件忽略大小写

--- app/src/main/java/com/alixlp/ship/bean/User.java
+++ app/src/main/java/com/alixlp/ship/bean/User.java
@@ -1,51 +1,16 @@
 package com.alixlp.ship.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private int id;
     private int kuaidiid; // 快遞單號
     private String token;
     private String password;
     private String username;
-
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public int getKuaidiid() {
-        return kuaidiid;
-    }
-
-    public void setKuaidiid(int kuaidiid) {
-        this.kuaidiid = kuaidiid;
-    }
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
